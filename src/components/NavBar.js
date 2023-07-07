c2e03c5300efa6dd71b85d1a53493a140a432024
jcjen62 <jcjensen62@gmail.com>
2023-07-06 18:10:43 -0600

    Intial commit and Navbar setup

---
+++ src/components/NavBar.js
@@ -0,0 +1,58 @@
+import * as React from "react";
+import Link from "next/link";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+
+const NavBar = () => {
+  return (
+    <>
+      <AppBar position="static" id="navBar">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              Jeremy Jensen
+            </Typography>
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: { xs: "none", md: "flex", justifyContent: "right" },
+              }}
+            >
+              <Button sx={{ my: 2, color: "white", display: "block" }}>
+                <Link href="/resume">Resume</Link>
+              </Button>
+
+              <Button sx={{ my: 2, color: "white", display: "block" }}>
+                <Link href="/projects">Projects</Link>
+              </Button>
+
+              <Button sx={{ my: 2, color: "white", display: "block" }}>
+                <Link href="/contact">Contact me</Link>
+              </Button>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </>
+  );
+};
+export default NavBar;
